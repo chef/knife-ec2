f6232d5d91f5d2239bee77a80eb89e14a2d788d0
Jacob Olsen <jakeo@urbanverb.com>
2011-09-27 13:10:54 -0700

    Add bootstrap_proxy [CHEF-2375].

--- lib/chef/knife/ec2_server_create.rb
+++ lib/chef/knife/ec2_server_create.rb
@@ -100,6 +100,11 @@ class Chef
         :description => "The version of Chef to install",
         :proc => Proc.new { |v| Chef::Config[:knife][:bootstrap_version] = v }
 
+      option :bootstrap_proxy,
+        :long => "--bootstrap-proxy PROXY_URL",
+        :description => "The proxy server for the node being bootstrapped",
+        :proc => Proc.new { |p| Chef::Config[:knife][:bootstrap_proxy] = p }
+
       option :distro,
         :short => "-d DISTRO",
         :long => "--distro DISTRO",
