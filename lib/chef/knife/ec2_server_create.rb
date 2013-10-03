01bb49ea1a4ef9c13f7dc6fcfd01047eb2d9343c
Gabriel Rosendorf <gabriel.rosendorf@weather.com>
2013-10-03 16:18:05 -0400

    added logging messages for public ips in vpc instances

--- lib/chef/knife/ec2_server_create.rb
+++ lib/chef/knife/ec2_server_create.rb
@@ -402,6 +402,9 @@ class Chef
 
         if vpc_mode?
           msg_pair("Subnet ID", @server.subnet_id)
+          if config[:associate_public_ip]
+            msg_pair("Public DNS Name", @server.dns_name)
+          end
           if elastic_ip
             msg_pair("Public IP Address", @server.public_ip_address)
           end
@@ -470,6 +473,9 @@ class Chef
         end
         if vpc_mode?
           msg_pair("Subnet ID", @server.subnet_id)
+          if config[:associate_public_ip]
+            msg_pair("Public DNS Name", @server.dns_name)
+          end
         else
           msg_pair("Public DNS Name", @server.dns_name)
           msg_pair("Public IP Address", @server.public_ip_address)
