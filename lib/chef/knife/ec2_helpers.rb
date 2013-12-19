65307279da859644d9068197c39d2c359316037e
prabhu-das <prabhu.das@clogeny.com>
2013-12-19 17:21:07 +0530

    Functional tests server list.

--- lib/chef/knife/ec2_helpers.rb
+++ lib/chef/knife/ec2_helpers.rb
@@ -16,6 +16,20 @@ class Chef
         end
 
         def validate!
+          errors = []
+          unless Chef::Config[:knife][:aws_credential_file].nil?
+            unless (Chef::Config[:knife].keys & [:aws_access_key_id, :aws_secret_access_key]).empty?
+              errors << "Either provide a credentials file or the access key and secret keys but not both."
+            end
+            # File format:
+            # AWSAccessKeyId=somethingsomethingdarkside
+            # AWSSecretKey=somethingsomethingcomplete
+            entries = Hash[*File.read(Chef::Config[:knife][:aws_credential_file]).split(/[=\n]/)]
+            Chef::Config[:knife][:aws_access_key_id] = entries['AWSAccessKeyId']
+            Chef::Config[:knife][:aws_secret_access_key] = entries['AWSSecretKey']
+            error_message = ""
+            raise CloudExceptions::ValidationError, error_message if errors.each{|e| ui.error(e); error_message = "#{error_message} #{e}."}.any?        
+          end
           super(:aws_access_key_id, :aws_secret_access_key)
         end
       end
