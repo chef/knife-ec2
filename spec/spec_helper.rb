613dd2552a5493f716cfa37d0c1afeca2242401a
prabhu-das <prabhu.das@clogeny.com>
2014-01-07 20:20:02 +0530

    Removed redundant TestResource class code.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,15 +3,4 @@ require 'chef/node'
 require 'fog'
 require 'chef/knife/ec2_server_create'
 require 'chef/knife/bootstrap_windows_ssh'
-
-class TestResource
-  def initialize(*args)
-    args.each do |arg|
-      arg.each do |key, value|
-        add_attribute = "class << self; attr_accessor :#{key}; end"
-        eval(add_attribute)
-        eval("@#{key} = value")
-      end
-    end
-  end
-end
+require 'resource_spec_helper'
