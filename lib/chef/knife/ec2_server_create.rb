0e1a86b3e621451f9ff446f38b3d0bd38f2d3d00
prabhu-das <prabhu.das@clogeny.com>
2014-01-09 19:34:16 +0530

    Display instance name and ID.

--- lib/chef/knife/ec2_server_create.rb
+++ lib/chef/knife/ec2_server_create.rb
@@ -100,7 +100,9 @@ class Chef
             hashed_tags["Name"] = locate_config_value(:chef_node_name) || @server.id
           end
 
-          @columns_with_info = [{:label => 'Flavor', :key => 'flavor_id'},
+          @columns_with_info = [{:label => 'Instance Name', :value => service.get_server_name(server)},
+                                {:label => 'Instance ID', :key => 'id'},
+                                {:label => 'Flavor', :key => 'flavor_id'},
                                 {:label => 'Image', :key => 'image_id'}, 
                                 {:label => 'Availability Zone', :key => 'availability_zone'},
                                 {:label => 'Public IP Address', :key => 'public_ip_address'},
