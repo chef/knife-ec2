0118832bb9c6a0e4d29f9e8e1a298b009467703b
Charles G <charles@chuckg.org>
2014-01-22 13:32:25 -0800

    Remove short option from aws_session_token (-T in use)

--- lib/chef/knife/ec2_base.rb
+++ lib/chef/knife/ec2_base.rb
@@ -52,7 +52,6 @@ class Chef
             :proc => Proc.new { |key| Chef::Config[:knife][:aws_secret_access_key] = key }
 
           option :aws_session_token,
-            :short => "-T TOKEN",
             :long => "--aws-session-token TOKEN",
             :description => "Your AWS Session Token, For Use With AWS STS Federation or Session Tokens",
             :proc => Proc.new { |key| Chef::Config[:knife][:aws_session_token] = key }
