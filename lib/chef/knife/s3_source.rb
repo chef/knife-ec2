715508f286d0820159b66d39902e5aba4a5333fa
Eric Herot <eric.github@herot.com>
2014-09-17 11:00:28 -0400

    Atomize the code a bit to make it more unit-test compatible

--- lib/chef/knife/s3_source.rb
+++ lib/chef/knife/s3_source.rb
@@ -5,12 +5,21 @@ class Chef
     class S3Source
       attr_accessor :url
 
+      def self.fetch(url)
+        source = Chef::Knife::S3Source.new
+        source.url = url
+        source.body
+      end
+
       def body
-        bucket_obj = fog.directories.get(bucket)
         bucket_obj.files.get(path).body
       end
 
       private
+
+      def bucket_obj
+        @bucket_obj ||= fog.directories.get(bucket)
+      end
 
       def bucket
         URI(@url).host
