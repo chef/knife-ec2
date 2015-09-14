b8f875d0eb998665f315e8d79c988f4f2d21c10d
NimishaS <nimisha.sharad@clogeny.com>
2015-09-14 19:34:50 +0530

    Added support for aws credential file

--- lib/chef/knife/ec2_helpers.rb
+++ lib/chef/knife/ec2_helpers.rb
@@ -5,7 +5,7 @@
 
 require 'chef/knife/cloud/ec2_service_options'
 require 'chef/knife/cloud/ec2_service'
-
+require 'pry'
 class Chef
   class Knife
     class Cloud
@@ -29,12 +29,10 @@ class Chef
               #               OR
               # aws_access_key_id = somethingsomethingdarkside
               # aws_secret_access_key = somethingsomethingdarkside
+              aws_creds = ini_parse(File.read(Chef::Config[:knife][:aws_credential_file]))
+              profile = Chef::Config[:knife][:aws_profile] || 'default'
+              entries = aws_creds.values.first.has_key?("AWSAccessKeyId") ? aws_creds.values.first : aws_creds[profile]
 
-              aws_creds = []
-              File.read(Chef::Config[:knife][:aws_credential_file]).each_line do | line |
-                aws_creds << line.split("=").map(&:strip) if line.include?("=")
-              end
-              entries = Hash[*aws_creds.flatten]
               Chef::Config[:knife][:aws_access_key_id] = entries['AWSAccessKeyId'] || entries['aws_access_key_id']
               Chef::Config[:knife][:aws_secret_access_key] = entries['AWSSecretKey'] || entries['aws_secret_access_key']
               error_message = ""
@@ -48,6 +46,28 @@ class Chef
           # The IAM profile object only contains the name as part of the arn
           name = profile['arn'].split('/')[-1] if profile && profile.key?('arn')
           name ||= ''
+        end
+
+        def ini_parse(file)
+          current_section = {}
+          map = {}
+          file.each_line do |line|
+             line = line.split(/^|\s;/).first # remove comments
+             section = line.match(/^\s*\[([^\[\]]+)\]\s*$/) unless line.nil?
+             if section
+               current_section = section[1]
+               binding.pry
+             elsif current_section
+               item = line.match(/^\s*(.+?)\s*=\s*(.+?)\s*$/) unless line.nil?
+               binding.pry
+               if item
+                 map[current_section] ||= {}
+                 map[current_section][item[1]] = item[2]
+                 binding.pry
+               end
+             end
+          end
+          map
         end
       end
     end
