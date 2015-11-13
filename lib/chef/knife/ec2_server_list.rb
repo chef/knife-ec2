f32822164326c685a7eddedab08ab5b51d9023d8
Vasu1105 <vasundhara.jagdale@clogeny.com>
2015-11-13 16:27:07 +0530

    server list in json format

--- lib/chef/knife/ec2_server_list.rb
+++ lib/chef/knife/ec2_server_list.rb
@@ -117,51 +117,55 @@ class Chef
           ui.warn "No region was specified in knife.rb or as an argument. The default region, us-east-1, will be used:"
         end
 
-        connection.servers.all.each do |server|
-          server_list << server.id.to_s
+        servers = connection.servers
+        if (config[:format] == 'summary')
+          servers.each do |server|
+            server_list << server.id.to_s
 
-          if config[:name]
-            server_list << server.tags["Name"].to_s
-          end
+            if config[:name]
+              server_list << server.tags["Name"].to_s
+            end
 
-          server_list << server.public_ip_address.to_s
-          server_list << server.private_ip_address.to_s
-          server_list << server.flavor_id.to_s
+            server_list << server.public_ip_address.to_s
+            server_list << server.private_ip_address.to_s
+            server_list << server.flavor_id.to_s
 
-          if config[:az]
-            server_list << ui.color(
-                                server.availability_zone.to_s,
-                                azcolor(server.availability_zone.to_s)
-                              )
-          end
+            if config[:az]
+              server_list << ui.color(
+                                  server.availability_zone.to_s,
+                                  azcolor(server.availability_zone.to_s)
+                                )
+            end
 
-          server_list << server.image_id.to_s
-          server_list << server.key_name.to_s
-          server_list << server.groups.join(", ")
+            server_list << server.image_id.to_s
+            server_list << server.key_name.to_s
+            server_list << server.groups.join(", ")
 
-          if config[:tags]
-            config[:tags].split(",").each do |tag_name|
-              server_list << server.tags[tag_name].to_s
+            if config[:tags]
+              config[:tags].split(",").each do |tag_name|
+                server_list << server.tags[tag_name].to_s
+              end
+            end
+
+            server_list << iam_name_from_profile(server.iam_instance_profile)
+
+            server_list << begin
+              state = server.state.to_s.downcase
+              case state
+              when 'shutting-down','terminated','stopping','stopped'
+                ui.color(state, :red)
+              when 'pending'
+                ui.color(state, :yellow)
+              else
+                ui.color(state, :green)
+              end
             end
           end
 
-          server_list << iam_name_from_profile(server.iam_instance_profile)
-
-          server_list << begin
-            state = server.state.to_s.downcase
-            case state
-            when 'shutting-down','terminated','stopping','stopped'
-              ui.color(state, :red)
-            when 'pending'
-              ui.color(state, :yellow)
-            else
-              ui.color(state, :green)
-            end
-          end
+          puts ui.list(server_list, :uneven_columns_across, output_column_count)
+        else
+          output(format_for_display(servers))
         end
-
-        puts ui.list(server_list, :uneven_columns_across, output_column_count)
-
       end
     end
   end
