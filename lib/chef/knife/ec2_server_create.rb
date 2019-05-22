0330716bf8e9092f9cf0adbe07b39997e484b4c3
Vivek Singh <vivek.singh@msystechnologies.com>
2019-05-22 11:24:37 +0530

    Refactor the server create bootstrap options  - Add common_options & connection_options modules.
    Signed-off-by: Vivek Singh <vivek.singh@msystechnologies.com>

--- lib/chef/knife/ec2_server_create.rb
+++ lib/chef/knife/ec2_server_create.rb
@@ -19,12 +19,17 @@
 
 require "chef/knife/ec2_base"
 require "chef/knife/s3_source"
+require "chef/knife/bootstrap/common_options"
+require "chef/knife/bootstrap/connection_options"
 
 class Chef
   class Knife
     class Ec2ServerCreate < Knife
 
       include Knife::Ec2Base
+      include Knife::Bootstrap::CommonOptions
+      include Knife::Bootstrap::ConnectionOptions
+
       deps do
         require "tempfile"
         require "uri"
@@ -116,69 +121,11 @@ class Chef
         description: "The Availability Zone",
         proc: Proc.new { |key| Chef::Config[:knife][:availability_zone] = key }
 
-      option :chef_node_name,
-        short: "-N NAME",
-        long: "--node-name NAME",
-        description: "The Chef node name for your new node",
-        proc: Proc.new { |key| Chef::Config[:knife][:chef_node_name] = key }
-
       option :ssh_key_name,
         short: "-S KEY",
         long: "--ssh-key KEY",
         description: "The AWS SSH key id",
         proc: Proc.new { |key| Chef::Config[:knife][:ssh_key_name] = key }
-
-      option :ssh_user,
-        short: "-x USERNAME",
-        long: "--ssh-user USERNAME",
-        description: "The ssh username",
-        default: "root"
-
-      option :ssh_password,
-        short: "-P PASSWORD",
-        long: "--ssh-password PASSWORD",
-        description: "The ssh password"
-
-      option :ssh_port,
-        short: "-p PORT",
-        long: "--ssh-port PORT",
-        description: "The ssh port",
-        default: "22",
-        proc: Proc.new { |key| Chef::Config[:knife][:ssh_port] = key }
-
-      option :ssh_gateway,
-        short: "-w GATEWAY",
-        long: "--ssh-gateway GATEWAY",
-        description: "The ssh gateway server. Any proxies configured in your ssh config are automatically used by default.",
-        proc: Proc.new { |key| Chef::Config[:knife][:ssh_gateway] = key }
-
-      option :ssh_gateway_identity,
-        long: "--ssh-gateway-identity IDENTITY_FILE",
-        description: "The private key for ssh gateway server",
-        proc: Proc.new { |key| Chef::Config[:knife][:ssh_gateway_identity] = key }
-
-      option :identity_file,
-        short: "-i IDENTITY_FILE",
-        long: "--identity-file IDENTITY_FILE",
-        description: "The SSH identity file used for authentication"
-
-      option :prerelease,
-        long: "--prerelease",
-        description: "Install the pre-release chef gems"
-
-      option :bootstrap_version,
-        long: "--bootstrap-version VERSION",
-        description: "The version of Chef to install",
-        proc: Proc.new { |v| Chef::Config[:knife][:bootstrap_version] = v }
-
-      option :bootstrap_proxy,
-        long: "--bootstrap-proxy PROXY_URL",
-        description: "The proxy server for the node being bootstrapped",
-        proc: Proc.new { |p| Chef::Config[:knife][:bootstrap_proxy] = p }
-
-      option :bootstrap_template,
-        long: "--bootstrap-template TEMPLATE",
-        description: "Bootstrap Chef using a built-in or custom template. Set to the full path of an erb template or use one of the built-in templates."
 
       option :ebs_size,
         long: "--ebs-size SIZE",
@@ -191,13 +138,6 @@ class Chef
       option :ebs_no_delete_on_term,
         long: "--ebs-no-delete-on-term",
         description: "Do not delete EBS volume on instance termination"
-
-      option :run_list,
-        short: "-r RUN_LIST",
-        long: "--run-list RUN_LIST",
-        description: "Comma separated list of roles/recipes to apply",
-        proc: lambda { |o| o.split(/[\s,]+/) },
-        default: []
 
       option :secret,
         long: "--secret ",
@@ -224,18 +164,6 @@ class Chef
         description: "allows to specify the private IP address of the instance in VPC mode",
         proc: Proc.new { |ip| Chef::Config[:knife][:private_ip_address] = ip }
 
-      option :host_key_verify,
-        long: "--[no-]host-key-verify",
-        description: "Verify host key, enabled by default.",
-        boolean: true,
-        default: true
-
-      option :bootstrap_protocol,
-        long: "--bootstrap-protocol protocol",
-        description: "protocol to bootstrap windows servers. options: winrm/ssh",
-        proc: Proc.new { |key| Chef::Config[:knife][:bootstrap_protocol] = key },
-        default: nil
-
       option :fqdn,
         long: "--fqdn FQDN",
         description: "Pre-defined FQDN. This is used for Kerberos Authentication purpose only",
@@ -248,15 +176,6 @@ class Chef
         description: "The EC2 User Data file to provision the instance with",
         proc: Proc.new { |m| Chef::Config[:knife][:aws_user_data] = m },
         default: nil
-
-      option :hint,
-        long: "--hint HINT_NAME[=HINT_FILE]",
-        description: "Specify Ohai Hint to be set on the bootstrap target.  Use multiple --hint options to specify multiple hints.",
-        proc: Proc.new { |h|
-          Chef::Config[:knife][:hints] ||= {}
-          name, path = h.split("=")
-          Chef::Config[:knife][:hints][name] = path ? JSON.parse(::File.read(path)) : Hash.new
-        }
 
       option :ephemeral,
         long: "--ephemeral EPHEMERAL_DEVICES",
@@ -287,11 +206,6 @@ class Chef
         description: "IOPS rate, only used when ebs volume type is 'io1'",
         proc: Proc.new { |key| Chef::Config[:knife][:provisioned_iops] = key },
         default: nil
-
-      option :auth_timeout,
-        long: "--windows-auth-timeout MINUTES",
-        description: "The maximum time in minutes to wait to for authentication over the transport to the node to succeed. The default value is 25 minutes.",
-        default: 25
 
       option :validation_key_url,
         long: "--validation-key-url URL",
@@ -327,76 +241,6 @@ class Chef
         description: "The maximum time in minutes to wait to for aws connection. Default is 10 min",
         proc: proc { |t| t = t.to_i * 60; Chef::Config[:aws_connection_timeout] = t },
         default: 600
-
-      option :node_ssl_verify_mode,
-        long: "--node-ssl-verify-mode [peer|none]",
-        description: "Whether or not to verify the SSL cert for all HTTPS requests.",
-        proc: Proc.new { |v|
-          valid_values = %w{none peer}
-          unless valid_values.include?(v)
-            raise "Invalid value '#{v}' for --node-ssl-verify-mode. Valid values are: #{valid_values.join(", ")}"
-          end
-        }
-
-      option :node_verify_api_cert,
-        long: "--[no-]node-verify-api-cert",
-        description: "Verify the SSL cert for HTTPS requests to the Chef server API.",
-        boolean: true
-
-      option :bootstrap_no_proxy,
-        long: "--bootstrap-no-proxy [NO_PROXY_URL|NO_PROXY_IP]",
-        description: "Do not proxy locations for the node being bootstrapped; this option is used internally by Opscode",
-        proc: Proc.new { |np| Chef::Config[:knife][:bootstrap_no_proxy] = np }
-
-      option :bootstrap_url,
-        long: "--bootstrap-url URL",
-        description: "URL to a custom installation script",
-        proc: Proc.new { |u| Chef::Config[:knife][:bootstrap_url] = u }
-
-      option :bootstrap_install_command,
-        long: "--bootstrap-install-command COMMANDS",
-        description: "Custom command to install chef-client",
-        proc: Proc.new { |ic| Chef::Config[:knife][:bootstrap_install_command] = ic }
-
-      option :bootstrap_wget_options,
-        long: "--bootstrap-wget-options OPTIONS",
-        description: "Add options to wget when installing chef-client",
-        proc: Proc.new { |wo| Chef::Config[:knife][:bootstrap_wget_options] = wo }
-
-      option :bootstrap_curl_options,
-        long: "--bootstrap-curl-options OPTIONS",
-        description: "Add options to curl when install chef-client",
-        proc: Proc.new { |co| Chef::Config[:knife][:bootstrap_curl_options] = co }
-
-      option :bootstrap_vault_file,
-        long: "--bootstrap-vault-file VAULT_FILE",
-        description: "A JSON file with a list of vault(s) and item(s) to be updated"
-
-      option :bootstrap_vault_json,
-        long: "--bootstrap-vault-json VAULT_JSON",
-        description: "A JSON string with the vault(s) and item(s) to be updated"
-
-      option :bootstrap_vault_item,
-        long: "--bootstrap-vault-item VAULT_ITEM",
-        description: 'A single vault and item to update as "vault:item"',
-        proc: Proc.new { |i|
-          (vault, item) = i.split(/:/)
-          Chef::Config[:knife][:bootstrap_vault_item] ||= {}
-          Chef::Config[:knife][:bootstrap_vault_item][vault] ||= []
-          Chef::Config[:knife][:bootstrap_vault_item][vault].push(item)
-          Chef::Config[:knife][:bootstrap_vault_item]
-        }
-
-      option :use_sudo_password,
-        long: "--use-sudo-password",
-        description: "Execute the bootstrap via sudo with password",
-        boolean: false
-
-      option :forward_agent,
-        short: "-A",
-        long: "--forward-agent",
-        description: "Enable SSH agent forwarding",
-        boolean: true
 
       option :create_ssl_listener,
         long: "--[no-]create-ssl-listener",
