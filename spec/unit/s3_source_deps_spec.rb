78bdc699b6d03b742d04c0faa09022d1ec6b272d
Vivek Singh <vivek.singh@msystechnologies.com>
2019-06-20 17:57:23 +0530

    Fix s3 source deps specs and remove flavor list specs
    Signed-off-by: Vivek Singh <vivek.singh@msystechnologies.com>

--- spec/unit/s3_source_deps_spec.rb
+++ spec/unit/s3_source_deps_spec.rb
@@ -3,19 +3,28 @@ require File.expand_path("../../spec_helper", __FILE__)
 # This spec can only be run separately from the rest due to inclusion of fog library in other specs.
 # rspec spec/unit/s3_source_deps_spec.rb
 
-describe "Check Dependencies", exclude: Object.constants.include?(:Fog) do
-  before(:each) do
-  end
-  it "should not load fog by default" do
+describe "Check Dependencies", exclude: Object.constants.include?(:Aws) do
+  it "should not load Aws::S3::Client by default" do
     begin
-      Fog::Storage::AWS.new()
+      Aws::S3::Client.new(conn)
     rescue Exception => e
       expect(e).to be_a_kind_of(NameError)
     end
   end
 
-  it "lazy loads fog" do
+  it "lazy loads Aws::S3::Client without required config" do
     begin
+      Chef::Knife::S3Source.fetch("test")
+    rescue Exception => e
+      expect(e).to be_a_kind_of(NoMethodError)
+    end
+  end
+
+  it "lazy loads Aws::S3::Client with required config" do
+    begin
+      Chef::Config[:knife][:aws_access_key_id] = "aws_access_key_id"
+      Chef::Config[:knife][:aws_secret_access_key] = "aws_secret_access_key"
+      Chef::Config[:knife][:region] = "test-region"
       Chef::Knife::S3Source.fetch("test")
     rescue Exception => e
       expect(e).to be_a_kind_of(ArgumentError)
